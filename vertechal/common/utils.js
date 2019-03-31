2ab866e607dd15b92e2cf90614a66b8fd33fba9f
msalomon90 <msalomon9@mail.fresnostate.edu>
2019-03-30 22:25:45 -0700

    add common and resource code

---
+++ vertechal/common/utils.js
@@ -0,0 +1,100 @@
+import document from "document";
+import { preferences } from "user-settings";
+import { battery } from "power";
+
+const time_label     = document.getElementById("time-text");
+const battery_bar    = document.getElementById("battery-bar");
+const percent_label  = document.getElementById("battery-percentage");
+//const battery_block = document.getElementById("battery-block-id");
+
+// Add zero in front of numbers < 10
+export function zeroPad(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+export function updateTime(evt)
+{
+  
+  let today = evt.date;
+  let hours = today.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = zeroPad(hours);
+  }
+  let mins = zeroPad(today.getMinutes());
+  let seconds = zeroPad(today.getSeconds());
+  time_label.text = `${hours}:${mins}`;
+}
+function resetBatteryBlocks(block_qty)
+{
+  console.log("reset battery");
+  let battery_block;
+  console.log(block_qty);
+  
+  for(let i = block_qty; i < 10; i++)
+  {
+    battery_block = document.getElementById("battery-block-" + i);
+    battery_block.style.fill = "#a4bcc1";
+  }
+}
+// Inserts new battery block in correct location inside the battery bar
+function insertBatteryBlock(full_block_qty)
+{           
+  let total_battery_blocks   = 10;  // each block is 10%
+  let active_block_opacity   = 10;
+  let inactive_block_opacity = 0.3;
+  
+  let block_start_y = 272;        // starting y coordinate of blocks (in pixels)
+  let block_pad     = 3;          // space between blocks
+  let block_height  = 25 + block_pad;   
+  
+  let battery_block;
+  
+  for(let i = 0; i < total_battery_blocks; i++)     
+  {
+    battery_block = document.getElementById("battery-block-" + i);
+    battery_block.y = block_start_y - (i * block_height);
+    
+    if(i < full_block_qty){
+      battery_block.style.opacity = active_block_opacity;
+    }
+    else{
+      //battery_block.style.visibility='hidden';
+      battery_block.style.opacity = inactive_block_opacity;
+    }
+    
+  }
+}
+
+// Updates the battery percentage blocks to reflect watch battery
+export function updateBattery()
+{
+  console.log("Updating battery bar...");
+  
+  let percentage       = Math.floor(battery.chargeLevel); // percentage from battery api
+  let remainder        = percentage % 10;                 // used for blocks less than 10% increments
+  let full_block_qty   = (percentage - remainder) / 10;   // number of full blocks
+  
+  percent_label.text = percentage + '%';
+  insertBatteryBlock(full_block_qty);
+  
+  if(remainder)    // insert a small block (percentage % 10 != 0)
+  {
+    let small_block   = remainder * 2.5;               // size of remainder block
+    let max_y         = 299;                           // max number of pixels vertically
+    let block_size    = 28;                            // size of block in pixels
+    let block_gap     = 3;                             // gap size between blocks
+    let battery_block = document.getElementById("battery-block-10");    // block to fill percentage overflow of next block
+    
+    battery_block.height = small_block;
+    battery_block.y = max_y - ((full_block_qty * block_size) + block_gap + small_block);   // location of overflow block   
+    battery_block.style.opacity = 10;  
+  }
+  
+}
