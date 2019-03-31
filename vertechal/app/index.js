0734e1a45efb73c4bdaafc28ad341e0e5b17d680
Michelle Salomon <msalomon@Michelles-MacBook-Pro.local>
2019-03-30 22:01:13 -0700

    import current app

--- vertechal/app/index.js
+++ vertechal/app/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+import clock from "clock";
+import * as util from "../common/utils";
+import { display } from "display";
+
+// Update the clock every minute
+clock.granularity = "minutes";
+// Get a handle on the <text> element
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => util.updateTime(evt);
+
+display.onchange = function() {
+    if (display.on) {
+      util.updateBattery();  
+    } else {
+      
+    }
+};
+
+
