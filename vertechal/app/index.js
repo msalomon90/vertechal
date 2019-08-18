43fa9901f2a6d7e1133d9e0494cdcebed5f4903a
msalomon90 <msalomon9@mail.fresnostate.edu>
2019-08-18 00:58:03 -0700

    moved updating functions to ontick evt listener

--- vertechal/app/index.js
+++ vertechal/app/index.js
@@ -1,21 +1,19 @@
 import clock from "clock";
 import * as util from "../common/utils";
 import { display } from "display";
+import { battery } from "power";
 
 // Update the clock every minute
 clock.granularity = "minutes";
 // Get a handle on the <text> element
 
 // Update the <text> element every tick with the current time
-clock.ontick = (evt) => util.updateTime(evt);
-
-display.onchange = function() {
-    if (display.on) {
-      util.updateBattery();  
-      util.updateSteps();
-    } else {
-      
-    }
+clock.ontick = (evt) => {
+  if (display.on) {
+    util.updateTime(evt);
+    util.updateBattery();  
+    util.updateSteps();
+  }
 };
 
 
